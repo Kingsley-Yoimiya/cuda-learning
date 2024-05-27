d7990733ff22c8dcfda6d5236635cc987f9c2e74
Kingsley-Yoimiya <153263432+Kingsley-Yoimiya@users.noreply.github.com>
2024-05-27 15:37:54 +0800

    Update Style Guide & Fix Code Style

--- STYLE_GUIDE.md
+++ STYLE_GUIDE.md
@@ -1,5 +1,5 @@
 #### C++
-C++ code should conform to [Google C++ Style Guide](https://google.github.io/styleguide/cppguide.html).
+C++ code should conform to [LLVM Style Guide]([LLVM Coding Standards (apple.com)](https://opensource.apple.com/source/lldb/lldb-112/llvm/docs/CodingStandards.html)).
 
 Addons uses [clang-format](https://clang.llvm.org/docs/ClangFormat.html)
 to check your C/C++ changes. Sometimes you have some manually formatted
@@ -24,7 +24,7 @@ sudo apt install clang-format-9
 
 format all with:
 ```bash
-clang-format-9 -i --style=google ./tensorflow_recommenders_addons/**/*.cc ./tensorflow_recommenders_addons/**/*.h
+clang-format-9 -i --style=LLVM simple_practice/*/*.cu
 ```
 
 Install Clang-format for MacOS:
@@ -35,53 +35,5 @@ brew install clang-format
 
 format all with:
 ```bash
-clang-format -i --style=google **/*.cc tensorflow_recommenders_addons/**/*.h
+clang-format -i --style=LLVM simple_practice/*/*.cu
 ```
-
-#### Python
-Recommenders Addons use [Yapf](https://github.com/google/yapf) to format our code.
-The continuous integration check will fail if you do not use it.
-
-Install them with:
-```
-pip install yapf
-```
-
-Be sure to run it before you push your commits, otherwise the CI will fail!
-
-```
-yapf --style=./.yapf -ir ./**/*.py
-```
-
-#### Bazel BUILD
-Use [buildifier](https://github.com/bazelbuild/buildtools/blob/master/buildifier/README.md) in project [bazelbuild/buildtools](https://github.com/bazelbuild/buildtools) to format the bazel code.
-
-Install it with:
-```bash
-git clone https://github.com/bazelbuild/buildtools.git
-cd buildtools
-bazel build //buildifier
-```
-Then copy the binary to directory on $PATH. (such as "/usr/local/bin")
-```bash
-cp bazel-bin/buildifier/buildifier_/buildifier /usr/local/bin
-```
-
-Run following commmand to see whether if installation ok:
-```bash
-buildifier --version
-```
-
-Use `buildifier`
-```bash
-buildifier -mode diff ${your_file_name}
-```
-to see formating problem in the BUILD file, or:
-```bash
-buildifier -mode diff ${directory}
-```
-for all BUILD files in ${directory}.
-
-#### TensorFlow Conventions
-
-Follow the guidance in the [TensorFlow Style Guide - Conventions](https://www.tensorflow.org/community/contribute/code_style#tensorflow_conventions_and_special_uses).
--- simple_practice/sort/sort.cu
+++ simple_practice/sort/sort.cu
@@ -1,122 +1,132 @@
-%%cuda
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
+#include <algorithm>
+#include <chrono>
+#include <ctime>
 #include <iostream>
 #include <random>
-#include <chrono>
-#include <cuda_runtime.h>
-#include <iostream>
-#include <ctime>
-#include <cuda.h>
-#include <assert.h>
-#include <algorithm>
 
 // using namespace std;
 
 #define GRID_SIZE 256
 #define BLOCK_SIZE 256
 #define BATCH 131072
-#define cudaCheckError() {                                                      \
-    cudaError_t err = cudaGetLastError();                                       \
-    if (err != cudaSuccess) {                                                   \
-        std::cerr << "CUDA error: " << cudaGetErrorString(err)                  \
-                  << " at " << __FILE__ << ":" << __LINE__ << std::endl;        \
-        exit(EXIT_FAILURE);                                                     \
-    }                                                                           \
-}
-#define cudaCheckErrorSync() {                                                  \
-    cudaDeviceSynchronize();                                                    \
-    cudaError_t err = cudaGetLastError();                                       \
-    if (err != cudaSuccess) {                                                   \
-        std::cerr << "CUDA error: " << cudaGetErrorString(err)                  \
-                  << " at " << __FILE__ << ":" << __LINE__ << std::endl;        \
-        exit(EXIT_FAILURE);                                                     \
-    }                                                                           \
-}
+#define cudaCheckError()                                                       \
+  {                                                                            \
+    cudaError_t err = cudaGetLastError();                                      \
+    if (err != cudaSuccess) {                                                  \
+      std::cerr << "CUDA error: " << cudaGetErrorString(err) << " at "         \
+                << __FILE__ << ":" << __LINE__ << std::endl;                   \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  }
+#define cudaCheckErrorSync()                                                   \
+  {                                                                            \
+    cudaDeviceSynchronize();                                                   \
+    cudaError_t err = cudaGetLastError();                                      \
+    if (err != cudaSuccess) {                                                  \
+      std::cerr << "CUDA error: " << cudaGetErrorString(err) << " at "         \
+                << __FILE__ << ":" << __LINE__ << std::endl;                   \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  }
 
 // #define cudaCheckErrorSync() {}
 // #define cudaCheckError() {}
 
 int nextPow2(int x) {
-    x--;
-    for(int i = 1; i < 32; i <<= 1) x |= x >> i;
-    return x + 1;
+  x--;
+  for (int i = 1; i < 32; i <<= 1)
+    x |= x >> i;
+  return x + 1;
 }
 
-__global__ void sort_kernel(const int* input, int* output, int batch, int n, int offset) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    int stb = id * batch, edb = stb + batch;
-    for(int id = stb; id < edb; id++) {
-        int st = id * offset * 2, mid = st + offset, ed = mid + offset;
-        st = min(st, n), mid = min(mid, n), ed = min(ed, n);
-        int pos = st, l = st, r = mid;
-        while(l < mid && r < ed) {
-            if(input[l] < input[r]) output[pos++] = input[l++];
-            else output[pos++] = input[r++];
-        }
-        while(l < mid) output[pos++] = input[l++];
-        while(r < ed)  output[pos++] = input[r++];
+__global__ void sort_kernel(const int *input, int *output, int batch, int n,
+                            int offset) {
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  int stb = id * batch, edb = stb + batch;
+  for (int id = stb; id < edb; id++) {
+    int st = id * offset * 2, mid = st + offset, ed = mid + offset;
+    st = min(st, n), mid = min(mid, n), ed = min(ed, n);
+    int pos = st, l = st, r = mid;
+    while (l < mid && r < ed) {
+      if (input[l] < input[r])
+        output[pos++] = input[l++];
+      else
+        output[pos++] = input[r++];
     }
+    while (l < mid)
+      output[pos++] = input[l++];
+    while (r < ed)
+      output[pos++] = input[r++];
+  }
 }
 // 需要优化，直接访问 output 全局数组理论上不快。
 
-void sort(std :: vector < int> &nums) {
-    int* cu_in;
-    int* cu_out;
-    int n = nums.size();
-    cudaMalloc((void**) &cu_in, n * sizeof(int));
-    cudaMalloc((void**) &cu_out, n * sizeof(int));
-    cudaMemcpy(cu_in, nums.data(), n * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemset(cu_out, 0, n * sizeof(int));
-    for(int offset = 1, tot = n + 1 >> 1; tot > 0; offset <<= 1, tot = tot + 1 >> 1) {
-        int totcore = min(tot, GRID_SIZE * BLOCK_SIZE);
-        int blocknum = min(totcore, BLOCK_SIZE);
-        int gridnum = (totcore + blocknum - 1) / blocknum;
-        int batch = (tot + blocknum * gridnum - 1) / (blocknum * gridnum);
-        // std :: cout << totcore << " " << blocknum << " " << gridnum << " " << batch << std :: endl;
-        sort_kernel <<< gridnum, blocknum >>> (cu_in, cu_out, batch, n, offset);
-        cudaDeviceSynchronize();
-        std :: swap(cu_in, cu_out);
-        if(tot == 1) break;
-    }
-    cudaMemcpy(nums.data(), cu_in,  n * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaFree(cu_in);
-    cudaFree(cu_out);
+void sort(std ::vector<int> &nums) {
+  int *cu_in;
+  int *cu_out;
+  int n = nums.size();
+  cudaMalloc((void **)&cu_in, n * sizeof(int));
+  cudaMalloc((void **)&cu_out, n * sizeof(int));
+  cudaMemcpy(cu_in, nums.data(), n * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemset(cu_out, 0, n * sizeof(int));
+  for (int offset = 1, tot = n + 1 >> 1; tot > 0;
+       offset <<= 1, tot = tot + 1 >> 1) {
+    int totcore = min(tot, GRID_SIZE * BLOCK_SIZE);
+    int blocknum = min(totcore, BLOCK_SIZE);
+    int gridnum = (totcore + blocknum - 1) / blocknum;
+    int batch = (tot + blocknum * gridnum - 1) / (blocknum * gridnum);
+    // std :: cout << totcore << " " << blocknum << " " << gridnum << " " <<
+    // batch << std :: endl;
+    sort_kernel<<<gridnum, blocknum>>>(cu_in, cu_out, batch, n, offset);
+    cudaDeviceSynchronize();
+    std ::swap(cu_in, cu_out);
+    if (tot == 1)
+      break;
+  }
+  cudaMemcpy(nums.data(), cu_in, n * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaFree(cu_in);
+  cudaFree(cu_out);
 }
 
 const int N = 5e8, M = 2000, K = 2000;
 const int T = 100;
 
-std :: uniform_real_distribution<float> u(0, 1);
-std :: mt19937 rnd(std :: chrono::system_clock::now().time_since_epoch().count());
+std ::uniform_real_distribution<float> u(0, 1);
+std ::mt19937 rnd(std ::chrono::system_clock::now().time_since_epoch().count());
 
 int main() {
-    std :: vector < int > a, b, c;
-    for(int i = 0; i < N; i++) {
-        a.emplace_back(rnd() % 1024);
+  std ::vector<int> a, b, c;
+  for (int i = 0; i < N; i++) {
+    a.emplace_back(rnd() % 1024);
+  }
+  b = a, c = a;
+  std ::cerr << "GENERATE OK!" << a.size() << std ::endl;
+
+  double st = clock();
+  sort(a);
+  double ed = clock();
+  std::cout << (ed - st) / CLOCKS_PER_SEC << std::endl;
+
+  // for(int i = 0; i < 10; i++) cout << a[i] << endl;
+
+  st = clock();
+  std ::sort(c.begin(), c.end());
+  ed = clock();
+  std::cout << (ed - st) / CLOCKS_PER_SEC << std::endl;
+
+  for (int i = 0; i < N; i++)
+    if (a[i] != c[i]) {
+      std ::cout << i << " " << a[i] << " " << c[i] << std ::endl;
+      break;
     }
-    b = a, c = a;
-    std :: cerr << "GENERATE OK!" << a.size() << std :: endl;
 
-    double st = clock();
-    sort(a);
-    double ed = clock();
-    std::cout << (ed - st) / CLOCKS_PER_SEC << std::endl;
-
-    // for(int i = 0; i < 10; i++) cout << a[i] << endl;
-
-    st = clock();
-    std :: sort(c.begin(), c.end());
-    ed = clock();
-    std::cout << (ed - st) / CLOCKS_PER_SEC << std::endl;
-
-    for(int i = 0; i < N; i++) if(a[i] != c[i]) {
-        std :: cout << i << " " << a[i] << " " << c[i] << std :: endl;
-        break;
-    }
-
-    std :: cout << a.size() << " " << c.size() << std :: endl;
-    assert(a == c);
-    return 0;
+  std ::cout << a.size() << " " << c.size() << std ::endl;
+  assert(a == c);
+  return 0;
 }
 /*
 GENERATE OK!500000000
